07be00b8cb8b63b18f053e359422d9607d242233
Christian Fischer <sw-dev@computerlyrik.de>
2014-03-02 03:00:21 +0100

    version up

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'chef-cookbooks@computerlyrik.de'
 license          'Apache 2.0'
 description      'Installs etherpad or etherpad-lite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 depends         'nodejs'
